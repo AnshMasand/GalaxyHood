89e83e27eab01234589de7b66005fcc9c2423b92
AnshMasand <63319005+AnshMasand@users.noreply.github.com>
2023-05-30 14:02:55 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 print(os.environ.get('DATABASE_URL'))
 
 class Config:
-    SECRET_KEY = 'lkasjdf09ajsdkfljalsiorj12n3490re9485309irefvn,u90818734902139489230'
+    SECRET_KEY = #enter key
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:QHIZD5X2lfJ43046YMQj@containers-us-west-66.railway.app:5809/railway'
+    SQLALCHEMY_DATABASE_URI = #enter uri
     SQLALCHEMY_ECHO = True
